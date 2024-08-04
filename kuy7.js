b7a4b40a09ff7e3fe0f8b8a5d66ff32f8cbf5220
Oleksii Vasin <vasin.oleksii@gmail.com>
2024-08-05 00:09:00 +0200

    kuy7

--- kuy7.js
+++ kuy7.js
@@ -1,7 +1,3 @@
-// Complete the solution so that it reverses the string passed into it.
-const solution = (str) => str.split("").reverse().join("");
-console.log(solution("Hell"));
-
 // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
 function descendingOrder(a) {
   return +(a + "")
